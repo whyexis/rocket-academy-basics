ced6a49568ab310099852359642f1dd176bc8ba0
whyexis <chongyingxiang@gmail.com>
2021-07-18 00:53:03 +0800

    Greetings app

---
+++ day1/pre-class/greetings/script.js
@@ -0,0 +1,10 @@
+// App Title
+var TITLE = "Greetings";
+
+// Instructions for app
+var DESCRIPTION = "User inputs name and the app returns a personalised greeting with user's name";
+
+// Program
+function main(input) {
+  return `Hello ${input}, welcome to the Basics Course!`;;
+}
