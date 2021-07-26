e9a273a1759b4c32558d235ca19378ca5871b2e9
whyexis <chongyingxiang@gmail.com>
2021-07-26 21:44:29 +0800

    Removed

--- day1/pre-class/greetings/script.js
+++
@@ -1,9 +0,0 @@
-// App Title
-var TITLE = "Greetings";
-var RATING = "Base"
-var DESCRIPTION = "User inputs name and the app returns a personalised greeting with user's name";
-
-// Program
-function main(input) {
-  return `Hello ${input}, welcome to the Basics Course!`;;
-}
