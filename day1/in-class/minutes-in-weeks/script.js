8967a17d94c968dfe573268f93becf918ff71c3e
whyexis <chongyingxiang@gmail.com>
2021-07-27 10:32:50 +0800

    Added minutes in weeks

---
+++ day1/in-class/minutes-in-weeks/script.js
@@ -0,0 +1,12 @@
+/**
+ * ------------------------------------------------------------------------
+ * Main
+ * ------------------------------------------------------------------------
+ */
+
+function main(input) {
+  var minutesInWeek = 10080;
+  var output = minutesInWeek * input;
+  var myOutputValue = `There are ${output} minutes in ${input} weeks! Wow!`;
+  return myOutputValue;
+}
