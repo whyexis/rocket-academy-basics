cd76cd2598a6b3e3383bc919e5f6e9669b79d61d
whyexis <chongyingxiang@gmail.com>
2021-07-28 19:57:05 +0800

    Added even easier dice game refactor

---
+++ day3/pre-class/even-easier-dice-game-refactor/script.js
@@ -0,0 +1,32 @@
+/**
+ * ------------------------------------------------------------------------
+ * Helper Functions
+ * ------------------------------------------------------------------------
+ */
+
+var diceRoll = function () {
+  var randomDecimal = Math.random() * 6;
+  var randomInteger = Math.floor(randomDecimal);
+  var diceNumber = randomInteger + 1;
+  return diceNumber;
+};
+
+/**
+ * ------------------------------------------------------------------------
+ * Main
+ * ------------------------------------------------------------------------
+ */
+
+function main(input) {
+  var randomDiceNumber = diceRoll();
+  var randomDiceParity = randomDiceNumber % 2;
+  var myOutputValue = `You guessed ${input}, but rolled ${randomDiceNumber}, you lose :(`;
+
+  if (
+    (input == "odd" && randomDiceParity == 1) ||
+    (input == "even" && randomDiceParity == 0)
+  ) {
+    myOutputValue = `You guessed ${input} and rolled ${randomDiceNumber}, you win :)`;
+  }
+  return myOutputValue;
+}
