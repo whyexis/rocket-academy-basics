b4e750613dbdd87744acea7ee884b0a139e913ee
whyexis <chongyingxiang@gmail.com>
2021-07-28 19:37:51 +0800

    Added secret words

---
+++ day3/pre-class/secret-words/script.js
@@ -0,0 +1,19 @@
+/**
+ * ------------------------------------------------------------------------
+ * Main
+ * ------------------------------------------------------------------------
+ */
+
+function main(input) {
+  var myOutputValue = "hello world";
+
+  if (
+    input == "neat noodles" ||
+    input == "awesome ayam" ||
+    input == "delicious dumplings" ||
+    input == "bananas"
+  ) {
+    var myOutputValue = "Not all who wander are lost.";
+  }
+  return myOutputValue;
+}
