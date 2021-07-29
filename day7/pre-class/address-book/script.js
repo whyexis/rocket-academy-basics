2db9efdd3bdb14b72b2a6ae970ef8554feabedac
whyexis <chongyingxiang@gmail.com>
2021-07-29 16:48:12 +0800

    Fixed bug

--- day7/pre-class/address-book/script.js
+++ day7/pre-class/address-book/script.js
@@ -14,28 +14,26 @@ var names = [];
  */
 
 function main(input) {
-  var main = function (input) {
-    // Set a boolean value found to a default value of false
-    var found = false;
+  // Set a boolean value found to a default value of false
+  var found = false;
 
-    // Loop over the names array, and set found to true if the input name already
-    // exists in the names array
-    var index = 0;
-    while (index < names.length) {
-      var currentName = names[index];
-      if (currentName == input) {
-        found = true;
-      }
-      index = index + 1;
+  // Loop over the names array, and set found to true if the input name already
+  // exists in the names array
+  var index = 0;
+  while (index < names.length) {
+    var currentName = names[index];
+    if (currentName == input) {
+      found = true;
     }
+    index += 1;
+  }
 
-    // If no duplicate name was found, add the input name to the names array
-    if (!found) {
-      names.push(input);
-    }
+  // If no duplicate name was found, add the input name to the names array
+  if (!found) {
+    names.push(input);
+  }
 
-    // Return the full array of names
-    var myOutputValue = "All names: " + names;
-    return myOutputValue;
-  };
+  // Return the full array of names
+  var myOutputValue = "All names: " + names;
+  return myOutputValue;
 }
