c2e36485df596bf756451ed2a9550b7f24d4c65e
whyexis <chongyingxiang@gmail.com>
2021-07-29 16:40:33 +0800

    Added address book

---
+++ day7/pre-class/address-book/script.js
@@ -0,0 +1,41 @@
+/**
+ * ------------------------------------------------------------------------
+ * Global Variables
+ * ------------------------------------------------------------------------
+ */
+
+// Initialise an empty names array
+var names = [];
+
+/**
+ * ------------------------------------------------------------------------
+ * Main
+ * ------------------------------------------------------------------------
+ */
+
+function main(input) {
+  var main = function (input) {
+    // Set a boolean value found to a default value of false
+    var found = false;
+
+    // Loop over the names array, and set found to true if the input name already
+    // exists in the names array
+    var index = 0;
+    while (index < names.length) {
+      var currentName = names[index];
+      if (currentName == input) {
+        found = true;
+      }
+      index = index + 1;
+    }
+
+    // If no duplicate name was found, add the input name to the names array
+    if (!found) {
+      names.push(input);
+    }
+
+    // Return the full array of names
+    var myOutputValue = "All names: " + names;
+    return myOutputValue;
+  };
+}
