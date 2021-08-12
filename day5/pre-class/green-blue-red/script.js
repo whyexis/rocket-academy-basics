a6090c24f869e0a77294d0fbda1e616b6b0b8657
whyexis <chongyingxiang@gmail.com>
2021-08-12 20:47:56 +0800

    Changed to submit event and optimised form input

--- day5/pre-class/green-blue-red/script.js
+++ day5/pre-class/green-blue-red/script.js
@@ -19,19 +19,21 @@ var main = function (input) {
     mode = "blue";
   } else if (input == "red") {
     mode = "red";
+  } else {
+    return `Please enter "green", "blue" or "red".`;
   }
 
   var myOutputValue =
-    "A fool sees not the same tree that a wise man sees. -William Blake";
+    "A fool sees not the same tree that a wise man sees.<br>-William Blake";
 
   if (mode == "blue") {
     myOutputValue =
-      "The sea, once it casts its spell, holds one in its net of wonder forever. -Jacques Cousteau";
+      "The sea, once it casts its spell, holds one in its net of wonder forever.<br>-Jacques Cousteau";
   }
 
   if (mode == "red") {
     myOutputValue =
-      "When the devil offers you a rose, its petals are laced with poison. -Matshona Dhliwayo";
+      "When the devil offers you a rose, its petals are laced with poison.<br>-Matshona Dhliwayo";
   }
 
   return myOutputValue;
